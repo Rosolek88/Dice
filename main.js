dcb3ddfd4a1e85abebfe7f111f2219e542c466e6
Administrator <administrator@CN-6.local>
2019-09-02 15:33:17 +0100

    2players

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const box = document.getElementById('box');
 const diceImg = document.getElementById('dice-img') //grabbing an img elemnt from html (space for img)
 let images = ["./img/dice1.png", "./img/dice2.png", "./img/dice3.png", "./img/dice4.png", "./img/dice5.png", "./img/dice6.png"]
 const lost = document.getElementById('lost');
-
+let total=0;
 
 function diceRoll() {
 
@@ -38,5 +38,53 @@ console.log();
 document.getElementById('button').onclick = function () {
 
     diceRoll(); //button click executes dice roll funcitioooooooon
+
+}
+
+////////////////////////////////////////////////// P L A Y E R R T W O////////////////////////////////////////////////////////
+
+
+
+
+let total11= 0;
+let total2 = 0;
+let sum1 = document.getElementById('sum1');
+const box1 = document.getElementById('box1');
+const diceImg1 = document.getElementById('dice-img1') //grabbing an img elemnt from html (space for img)
+//let images1 = ["./img/dice1.png", "./img/dice2.png", "./img/dice3.png", "./img/dice4.png", "./img/dice5.png", "./img/dice6.png"]
+const lost1 = document.getElementById('lost1');
+
+function diceRoll1() {
+
+
+    let randomNumber1 = Math.floor(Math.random() * 6 + 1); // randomises number between 1-6
+    let total2 = randomNumber1; // total to be random number
+
+    //box.textContent = total; // shows number in the box
+    total2 = total11 += total2; // add all random numbers in total1
+    sum1.textContent = total2; // shows numbers in sum box
+    if (total2 == 1) {
+        lost1.textContent = "You Lose!"
+        total2 = 0;
+    } // if total equals 1 than you lose 
+    if (total11 >= 21) {
+        sum1.textContent = "You Won!"
+        total11 = 0; // if total1 sum equals 21 or more you win
+    }
+    diceImg1.src = images[total2 - 1];
+}
+console.log();
+
+
+
+
+
+
+
+
+
+document.getElementById('button1').onclick = function () {
+
+    diceRoll1(); //button click executes dice roll funcitioooooooon
 
 }
