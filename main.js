137813a38a63e5751269d6371247e9e95f5f8229
Administrator <administrator@CN-6.local>
2019-09-02 15:03:46 +0100

    update working

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let total1 = 0;
 let sum = document.getElementById('sum');
 const box = document.getElementById('box');
-//const images = [dice1.png, dice2.png, dice3.png, dice4.png, dice5.png, dice6.png]
-
+const diceImg = document.getElementById('dice-img') //grabbing an img elemnt from html (space for img)
+let images = ["./img/dice1.png", "./img/dice2.png", "./img/dice3.png", "./img/dice4.png", "./img/dice5.png", "./img/dice6.png"]
+const lost = document.getElementById('lost');
 
 
 function diceRoll() {
@@ -10,19 +11,22 @@ function diceRoll() {
 
     let randomNumber = Math.floor(Math.random() * 6 + 1); // randomises number between 1-6
     let total = randomNumber; // total to be random number
-    box.textContent = total; // shows number in the box
+
+    //box.textContent = total; // shows number in the box
     total1 = total1 += total; // add all random numbers in total1
     sum.textContent = total1; // shows numbers in sum box
     if (total == 1) {
-        box.textContent = "You Lose!"
-        total=0;} // if total equals 1 than you lose 
-        if (total1 >=  21) {
-            sum.textContent = "You Won!"
-            total1=0; // if total1 sum equals 21 or more you win
-        }
-       // image.scr = `./img $(images[random - 1])`;
+        lost.textContent = "You Lose!"
+        total = 0;
+    } // if total equals 1 than you lose 
+    if (total1 >= 21) {
+        sum.textContent = "You Won!"
+        total1 = 0; // if total1 sum equals 21 or more you win
     }
-    console.log();
+    diceImg.src = images[total - 1];
+}
+console.log();
+
 
 
 
@@ -33,6 +37,6 @@ function diceRoll() {
 
 document.getElementById('button').onclick = function () {
 
-    diceRoll();
+    diceRoll(); //button click executes dice roll funcitioooooooon
 
 }
